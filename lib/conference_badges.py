f40cbe4238ff02f92667e10c570826d15930d1b9
Racquel Glickman <racquel.glickman@gmail.com>
2023-05-02 17:11:00 -0400

    completes lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,14 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]
 
 def printer(names):
-    return None
+    for entry in batch_badge_creator(names):
+        print(entry)
+    for entry in assign_rooms(names):
+        print(entry)
